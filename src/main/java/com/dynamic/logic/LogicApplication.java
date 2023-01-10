a67e01ff69b5f573384d5964a27473be7ffc6ea2
Abdul Renish <arenishr@gmail.com>
2023-01-10 22:39:03 +0300

    new development

---
+++ src/main/java/com/dynamic/logic/LogicApplication.java
@@ -0,0 +1,29 @@
+package com.dynamic.logic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicApplication.class, args);
+		
+		
+	}
+	
+	private int tallestStack() {
+		
+		
+		return 0;
+		
+	}
+	
+	private boolean canBeStacked() {
+		
+		
+		return false;
+		
+	}
+
+}
---
+++ src/test/java/com/dynamic/logic/LogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamic.logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
